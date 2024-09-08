e7a222ae4c940060c4d3d2081ddf154590236025
robin-UI <robinbiju1901@gmail.com>
2024-09-08 13:56:17 +0530

    study variables

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
-use std::io;
 use rand::Rng;
+use std::{io, u32};
+
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
 
     let screat_number: i32 = rand::thread_rng().gen_range(1..=100);
 
     println!("our guessed number is {screat_number}.");
-    let mut guess: String = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("fail to user input");
+    loop {
+        println!("Please input your guess.");
+        let mut guess: String = String::new();
 
-    println!("Your guess number is {guess}")
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("fail to user input");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(err) => {
+                continue;
+            },
+        }; //Shadowing
+
+        match guess.cmp(&screat_number) {
+            Ordering::Equal => println!("You Won"),
+            Ordering::Greater => println!("To Big"),
+            Ordering::Less => println!("To small"),
+        }
+    }
 }
